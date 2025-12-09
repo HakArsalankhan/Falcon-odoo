276842b5b2acd2bf898921def8313cad575cf835
Fazeel Malik <fazeelmalik444@gmail.com>
2025-12-09 10:52:12 +0500

    custom_pos

---
+++ custom_purchase/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import quotation
+from . import purchase_order
+from . import delivery_note
+from . import rfq
+from . import purchase_order_line
